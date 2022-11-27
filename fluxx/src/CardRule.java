e917fab8611455d893a1ad6ec31b5d5256b5381e
1822913 <peterkoegler@gmx.de>
2022-11-27 14:29:22 +0100

    start implementing game class, init rule cards

--- fluxx/src/CardRule.java
+++ fluxx/src/CardRule.java
@@ -1,7 +1,8 @@
 
-public abstract class CardRule extends Card {
+public class CardRule extends Card {
 	private String which = "";
 	private int limit;
+	
 	public CardRule(String which, int limit) {
 		this.which = which;
 		this.limit = limit;
--- fluxx/src/Game.java
+++ fluxx/src/Game.java
@@ -1,4 +1,54 @@
+import java.util.List;
+import java.util.ArrayList;
 
 public class Game {
+	private static final int MINPLAYER = 2;
+	private static final int MAXPLAYER = 6;
+	
+	private List<Card> deck;
+	private List<Card> discardedPile;
+	private List<Card> drawStack;
+	private List<Player> players;
+	
+	public Game(int num_players) {
+		deck = new ArrayList<>();
+		discardedPile = new ArrayList<>();
+		drawStack = new ArrayList<>();
+		players = new ArrayList<>();
+		initPlayers(num_players);
+		initCards();
+	}
+	
+	private void initPlayers(int players) {
+		
+	}
+	
+	private void initCards() {
+		// rule cards
+		deck.add(new CardRule("draw", 2));
+		deck.add(new CardRule("draw", 3));
+		deck.add(new CardRule("draw", 4));
+		deck.add(new CardRule("draw", 5));
+		
+		deck.add(new CardRule("keeper", 2));
+		deck.add(new CardRule("keeper", 3));
+		deck.add(new CardRule("keeper", 4));
 
+		deck.add(new CardRule("play", 2));
+		deck.add(new CardRule("play", 3));
+		deck.add(new CardRule("play", 4));
+		deck.add(new CardRule("play", -1));
+		
+		deck.add(new CardRule("hand", 0));
+		deck.add(new CardRule("hand", 1));
+		deck.add(new CardRule("hand", 2));
+		
+		// keeper cards
+		 
+		// goal cards
+
+		
+		
+		
+	}
 }
