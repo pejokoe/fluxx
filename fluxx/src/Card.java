071c9eb02f0d5d86416f40e9c33b3fbf00573a47
taniafug <92566861+taniafug@users.noreply.github.com>
2022-11-28 21:45:59 +0100

    Update Card.java

--- fluxx/src/Card.java
+++ fluxx/src/Card.java
@@ -2,6 +2,13 @@
 public abstract class Card {
 	protected int idCard;
 	
+	//I ADDED
+	protected static String namecard;
+	protected static String descriptioncard;
+	protected static String typecard;
+	protected static int totalquantitycard;
+	//////////
+	
 	public Card(int idCard) {
 		this.idCard = idCard;
 	}
@@ -11,4 +18,35 @@ public abstract class Card {
 	}
 	
 	abstract public void playCard(RuleArea ruleArea);
+	// I ADDED
+	public Card(int idcard, String namecard,String descriptioncard, String typecard, int totalquantitycard)
+	{
+	this.idcard=idcard;
+	this.namecard=namecard;
+	this.descriptioncard=descriptioncard;
+	this.typecard=typecard;
+	this.totalquantitycard=totalquantitycard;
+	}
+	public String getnamecard()
+	{
+	return namecard;
+	}
+	public String getdescriptioncard()
+	{
+	return descriptioncard;
+	}
+	public String gettypecard()
+	{
+	return typecard;
+	}
+	public int gettotalquantitycard()
+	{
+	return totalquantitycard;
+	}
+
+	public void suffle()
+	{
+	
+	}
+	////////////
 }
