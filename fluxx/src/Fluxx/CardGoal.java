9f4da28013b01329ad0f9d76939fae52839c5111
taniafug <92566861+taniafug@users.noreply.github.com>
2023-01-03 18:23:43 +0100

    Update CardGoal.java
    Refining

--- fluxx/src/Fluxx/CardGoal.java
+++ fluxx/src/Fluxx/CardGoal.java
@@ -1,5 +1,5 @@
 package Fluxx;
-//LAST CHANGE 2/01/2023
+
 /* This class will be in charge of representing the Goal card.
  * Each GoalCard will be composed by two 'cardkeepers'.
  * Another type of Goal card is managed trough using polimorphism as having to constructor 
@@ -15,45 +15,58 @@ public class CardGoal extends Card
 	private CardKeeper keeper2;
 	private int special;
 
-// First constructor for card goals composed by card keepers.
+	// First constructor for card goals composed by card keepers.
 	public CardGoal(int idCard, CardKeeper keeper1, CardKeeper keeper2) 
 	{
 		super(idCard, keeper1.getNameCard() + " + " + keeper2.getNameCard());
 		this.keeper1 = keeper1;
 		this.keeper2 = keeper2;
 	}
-//Second constructor for special card goals.
+	//Second constructor for special card goals.
 	public CardGoal(String name, int idCard, int special) {
 		super(idCard, name);
 		this.special=special;
 	}
 
-// Method to obtain the first card keeper composing the card goal.
+	// Method to obtain the first card keeper composing the card goal.
 	public CardKeeper getKeeper1() 
 	{
 		return keeper1;
 	}
-// Method to obtain the second card keeper composing the card goal.
+	
+	// Method to obtain the second card keeper composing the card goal.
 	public CardKeeper getKeeper2() 
 	{
 		return keeper2;
 	}
-// Method to obtain the number of card keepers that will be necessary to play a defined special goal card.
+	
+	// Method to obtain the number of card keepers that will be necessary to play a defined special goal card.
 	public int special()
 	{
 		return special;
 	}
-//Method used hen a Goal card is played in class game.
+	
+	//Method used hen a Goal card is played in class game.
 	public void playCard(Game game)
 	{
 		game.playGoal(this);
 	}
-//Basic display method.
+	
+	//Basic display method.
 	public String display() 
 	{
-		return " Goal " + this.getNameCard();
+		if(this.getNameCard().contains(" keepers (S)"))
+		{
+			return "Special goal" + this.getNameCard();
+		}
+		else
+		{
+			return "Goal " + this.getNameCard();
+		}
+		
 	}
-//Display extended method for both: Card goals composed by card keepers and Special Card Goals.
+	
+	//Display extended method for both: Card goals composed by card keepers and Special Card Goals.
 	public String display_extended() 
 	{
 		if(this.getNameCard().contains(" keepers (S)"))
